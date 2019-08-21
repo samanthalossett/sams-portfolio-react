fb7120d23b7b1b0f0f47116f53e56be8b795e13d
SamanthaLossett <samanthalossett@gmail.com>
2019-08-21 14:48:52 -0700

    added a time function to say good morning/afternoon/evening to the viewer

--- src/components/MainContent/index.js
+++ src/components/MainContent/index.js
@@ -2,8 +2,21 @@ import React from "react";
 import "./style.css";
 
 function MainContent(){
+
+    const date= new Date();
+    const hours= date.getHours();
+    let timeOfDay
+
+    if (hours <12){
+        timeOfDay= "Morning"
+    }else if (hours > 12 && hours < 17){
+        timeOfDay="Afternoon"
+    }else {
+        timeOfDay="Night"
+    }
+
     return(
-        <div className="mainContentDiv">This is where the main stuff goes</div>
+        <div className="mainContentDiv">Good {timeOfDay}, and thanks for viewing my portfolio!</div>
     )
 }
 export default MainContent;
